f7f8cd3fd6769e28aed88897bde6010a8899ae90
Adityava Gangopadhyay <adityava49cse@gmail.com>
2026-06-11 22:13:24 +0530

    fix login_view routing endpoint

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def create_app():
     db.init_app(app)
 
     login_manager = LoginManager()
-    login_manager.login_view = 'main.login'
+    login_manager.login_view = 'main.index'
     login_manager.init_app(app)
 
     @login_manager.user_loader
